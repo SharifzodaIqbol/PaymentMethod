95abc3ecbbb032f0deafaab2cc4ac6cb2d348b9a
Шарифзода Икбол <iqbolsharifzoda724@gmail.com>
2026-01-28 11:11:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+interface PaymentMethod{
+    void pay(double amount);
+}
+class BankCardPayment implements PaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата картой:" + amount);
+    }
+}
+
+class CashPayment implements PaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата наличными:" + amount);
+    }
+}
+class CryptoPayment implements PaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата криптовалютой:" + amount);
+    }
+}
+
+class ServicePayment{
+    public void processPayment(PaymentMethod method, double totalSum) {
+        method.pay(totalSum);
+        System.out.println("Заказ успешно оплачен!");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        ServicePayment service = new ServicePayment();
+
+        PaymentMethod myCard = new BankCardPayment();
+        service.processPayment(myCard, 1500.0);
+
+        PaymentMethod ton = new CryptoPayment();
+        service.processPayment(ton, 2000.0);
+    }
+}
